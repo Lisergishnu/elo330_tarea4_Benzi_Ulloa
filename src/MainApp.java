d295eb2f31acb5f0e22be9d2a679c88c11573c18
Lisergishnu <marco.benzi@alumnos.usm.cl>
2014-11-22 15:19:04 -0300

    Added main entry point and argument parsing

---
+++ src/MainApp.java
@@ -0,0 +1,29 @@
+/**
+ * Entry point to erp_udp application. Takes argument inputs and prepares threads for delay as expected.
+ * @author mbenzit
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		int delay_avg, delay_variation, loss_percent, local_port, remote_port;
+		String remote_host;
+		if (args.length != 6 || args.length != 7) {
+			System.out.println("Usage: erp_udp [delay_avg] [delay_variation] [loss_percent] "
+					+ "[local_port] (remote_host) [remote_port]");
+			System.exit(0);
+		}
+		delay_avg = Integer.parseInt(args[1]);
+		delay_variation = Integer.parseInt(args[2]);
+		loss_percent = Integer.parseInt(args[3]);
+		local_port = Integer.parseInt(args[4]);
+		if (args.length == 6) {
+			remote_host = "127.0.0.1";
+			remote_port = Integer.parseInt(args[5]);
+		} else {
+			remote_host = args[5];
+			remote_port = Integer.parseInt(args[6]);
+		}
+	}
+
+}
