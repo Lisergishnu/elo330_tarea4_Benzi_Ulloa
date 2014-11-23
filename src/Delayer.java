0ebef0627b2149716ab9f3b60b8780aad445aeaa
yuyomagico <andresjk99@gmail.com>
2014-11-23 00:56:50 -0300

    codigo agregado

--- src/Delayer.java
+++ src/Delayer.java
@@ -3,8 +3,14 @@
  * @author mbenzit
  *
  */
+import java.io.IOException;
+import java.net.*;
+
 public class Delayer {
 
+	private int BUFFER_SIZE = 50;
+	private int BUFFER_LENGTH = 1024;
+	
 	private String local_host;
 	private int local_port;
 	private String remote_host;
@@ -12,13 +18,19 @@ public class Delayer {
 	private int delay_avg;
 	private int delay_variation;
 	private int loss_percent;
+	protected byte[][] circularBuffer;
+	protected int sIndex, rIndex;
+	protected DatagramSocket socket;
+	
+	private String sync = new String();
+	private boolean newPacket = false;
 	
 	private Thread sender;
 	private Thread reciever;
 
 	public Delayer(String local_host, int local_port, String remote_host,
 			int remote_port, int delay_avg, int delay_variation,
-			int loss_percent) {
+			int loss_percent) throws SocketException  {
 				this.local_host = local_host;
 				this.local_port = local_port;
 				this.remote_host = remote_host;
@@ -26,13 +38,19 @@ public class Delayer {
 				this.delay_avg = delay_avg;
 				this.delay_variation = delay_variation;
 				this.loss_percent = loss_percent;
-				
+
+			socket = new DatagramSocket(local_port);
+			circularBuffer = new byte[BUFFER_SIZE][];
 			SenderThread s = new SenderThread();
 			RecieverThread r = new RecieverThread();
+			sIndex = 0;
+			rIndex = 0;
 			sender = new Thread(s);
 			sender.start();
 			reciever = new Thread(r);
 			reciever.start();
+
+			System.out.println("Listening port: " + local_port + " Dest port: " + remote_port);
 	}
 
 	/**
@@ -45,21 +63,74 @@ public class Delayer {
 	}
 	
 	class SenderThread implements Runnable {
-
+		private DatagramPacket packet = null;		
+		
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
-			
+			while(true){
+				sendPacket();
+			}
+		}
+		private void sendPacket(){
+	        try {
+	        	synchronized(sync){
+	        		if(!newPacket)
+	        			sync.wait();
+	        		newPacket = false;
+	        		sync.notify();
+	        	}
+        		Thread.sleep(1000);
+	        	synchronized(sync){
+	        		if(circularBuffer[sIndex] != null){
+						System.out.println("Sending packet: " + new String(circularBuffer[sIndex]));
+				        try {
+							packet = new DatagramPacket(circularBuffer[sIndex], BUFFER_LENGTH, InetAddress.getByName(remote_host), remote_port);
+							socket.send(packet);
+							circularBuffer[sIndex] = null;
+					        sIndex = (sIndex+1) % BUFFER_SIZE;
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+					}
+	        	}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	class RecieverThread implements Runnable {
-
+		private DatagramPacket packet = null;		
+		
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
-			
+			while(true){
+				recvPacket();
+			}
 		}
-		
+		private void recvPacket(){
+	        try {
+	        	synchronized(sync){
+	        		if(newPacket)
+	        			sync.wait();
+	        		if(circularBuffer[rIndex] != null) {
+		        		System.out.println("Buffer override");
+		        		sIndex = (sIndex+1) % BUFFER_SIZE;
+		        	}
+	        		else{
+			        	byte[] tempBuffer = new byte[BUFFER_LENGTH];
+						packet = new DatagramPacket(tempBuffer, BUFFER_LENGTH);
+				        socket.receive(packet);
+				        circularBuffer[rIndex] = tempBuffer;
+				        rIndex = (rIndex+1) % BUFFER_SIZE;
+						System.out.println("Receiving packet: " + new String(tempBuffer));
+	        		}
+	        		newPacket = true;
+	        		sync.notify();
+	        	}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}		
 	}
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,3 +1,6 @@
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  * Entry point to erp_udp application. Takes argument inputs and prepares threads for delay as expected.
  * @author mbenzit
@@ -8,33 +11,34 @@ public class MainApp {
 	public static void main(String[] args) {
 		int delay_avg, delay_variation, loss_percent, local_port, remote_port;
 		String remote_host;
-		if (args.length != 6 || args.length != 7) {
+		if (args.length != 5 && args.length != 6) {
 			System.out.println("Usage: erp_udp [delay_avg] [delay_variation] [loss_percent] "
 					+ "[local_port] (remote_host) [remote_port]");
 			System.exit(0);
 		}
-		delay_avg = Integer.parseInt(args[1]);
-		delay_variation = Integer.parseInt(args[2]);
-		loss_percent = Integer.parseInt(args[3]);
-		local_port = Integer.parseInt(args[4]);
-		if (args.length == 6) {
+		delay_avg = Integer.parseInt(args[0]);
+		delay_variation = Integer.parseInt(args[1]);
+		loss_percent = Integer.parseInt(args[2]);
+		local_port = Integer.parseInt(args[3]);
+		if (args.length == 5) {
 			remote_host = "127.0.0.1";
-			remote_port = Integer.parseInt(args[5]);
+			remote_port = Integer.parseInt(args[4]);
 		} else {
-			remote_host = args[5];
-			remote_port = Integer.parseInt(args[6]);
+			remote_host = args[4];
+			remote_port = Integer.parseInt(args[5]);
 		}
-		
 		//Generate delayers in both ways (two instances of the same object)
-		Delayer outward = new Delayer("127.0.0.1",local_port,remote_host,remote_port,delay_avg,delay_variation,loss_percent);
-		Delayer inward = new Delayer(remote_host,remote_port,"127.0.0.1",local_port,delay_avg,delay_variation,loss_percent);
+		Delayer outward;
+		Delayer inward;
 		
+		/* Try catch para socketException y el thread join*/
 		try {
+			outward = new Delayer("127.0.0.1",local_port,remote_host,remote_port,delay_avg,delay_variation,loss_percent);
+			//inward = new Delayer(remote_host,remote_port+1,"127.0.0.1",local_port+3,delay_avg,delay_variation,loss_percent);
 			outward.join();
-			inward.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//inward.join();
+		} catch (Exception e1) {
+			e1.printStackTrace();
 		}
 	}
 }
